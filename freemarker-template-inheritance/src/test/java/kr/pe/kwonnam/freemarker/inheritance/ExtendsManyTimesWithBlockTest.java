fbe568d98e426247e2e7d44537f64d230a79882f
Matteo Silvestri <matteo.silvestri@eidosmedia.com>
2017-10-26 10:56:26 +0200

    Test restored for multi level inheritance
    Test restored for multi level inheritance with block redefined at each
    level

---
+++ freemarker-template-inheritance/src/test/java/kr/pe/kwonnam/freemarker/inheritance/ExtendsManyTimesWithBlockTest.java
@@ -0,0 +1,25 @@
+package kr.pe.kwonnam.freemarker.inheritance;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * User: KwonNam Son(kwon37xi@gmail.com}
+ * Date: 13. 6. 30
+ * Time: 오후 11:43
+ */
+public class ExtendsManyTimesWithBlockTest extends AbstractDirectiveTest {
+
+    @Test
+    public void extends_many_times_with_block() {
+        String result = processTemplate("extends_many_times_with_block.ftl");
+
+        assertThat("다차 상속", result, is("{[FirstBlock-FirstBlockChild1-FirstAppendBlockChild2]" +
+                "[SecondBlock-SecondBlockChildReplace]" +
+                "[ThirdBlock-ThirdBlockChildPrepend2-ThirdBlockChild1]" +
+                "[FourthBlock-FourthBlockChildAppend2]" +
+                "[FifthBlock-FifthBlockChild1]}"));
+    }
+}
